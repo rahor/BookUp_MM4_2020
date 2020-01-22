51a4cbd451713971c1bb457740f7624a6c1e7ed0
rahor <hor.raphaell@gmail.com>
2020-01-22 18:53:41 +0100

    indent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-var step1 = document.querySelector('[data-id="1"]');
-var step2 = document.querySelector('[data-id="2"]');
-var step3 = document.querySelector('[data-id="3"]');
+var step0= document.querySelector('[data-id="1"]');
+var step1 = document.querySelector('[data-id="2"]');
+var step2 = document.querySelector('[data-id="3"]');
+var rond = document.getElementsByClassName('tunnel_rond');
 
 var step_button = document.querySelector('[data-button="1"]')
 
@@ -8,8 +9,14 @@ var step_button = document.querySelector('[data-button="1"]')
 
 
 step_button.addEventListener('click',()=>{
-  step1.style.opacity = "0";
-  step2.style.opacity = "1";
-  step1.style.pointerEvents = "none";
-  step2.style.pointerEvents = "auto";
+  for(var i=0;i<rond.length;i++){
+    if(rond[i].classList.contains("active")){
+      rond[i].classList.remove("active");
+      rond[i+1].classList.add("active");
+      step1.style.opacity = "1";
+      step1.style.pointerEvents = "auto";
+      step0.style.opacity = "0";
+      step0.style.pointerEvents = "none";
+    }
+  }
 })
