4b04d3babcc1419cbc089f92d2ba1529096f4e29
rahor <hor.raphaell@gmail.com>
2020-01-23 23:18:50 +0100

    Add JS Part v0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,62 @@
-var step0= document.querySelector('[data-id="1"]');
-var step1 = document.querySelector('[data-id="2"]');
-var step2 = document.querySelector('[data-id="3"]');
+
+
+var steps = [
+  step0 = document.querySelector('[data-id="1"]'),
+  step1 = document.querySelector('[data-id="2"]'),
+  step2 = document.querySelector('[data-id="3"]'),
+]
+
+// Rond/Etape du tunnel
 var rond = document.getElementsByClassName('tunnel_rond');
 
-var step_button = document.querySelector('[data-button="1"]')
+// Boutton de confirmation
+var step_button = document.getElementsByClassName('bouton_confirmation')
 
+//Mode de retrait
+var retraitCarte = document.getElementsByClassName("retrait_carte");
 
-
-
-step_button.addEventListener('click',()=>{
-  for(var i=0;i<rond.length;i++){
-    if(rond[i].classList.contains("active")){
-      rond[i].classList.remove("active");
-      rond[i+1].classList.add("active");
-      step1.style.opacity = "1";
-      step1.style.pointerEvents = "auto";
-      step0.style.opacity = "0";
-      step0.style.pointerEvents = "none";
-    }
+// gestion de la sélection du mode de retrait
+for (var l = 0; l < retraitCarte.length; l++) {
+  retraitCarte[l].addEventListener("click", function() {
+  var selected = document.getElementsByClassName("selected");
+  if (selected.length > 0) { 
+    selected[0].className = selected[0].className.replace(" selected", "");
   }
-})
+  this.className += " selected";
+
+  // On autorise le clic sur le bouton si un mode de retrait a été sélectionné
+  step_button[0].style.pointerEvents = "auto"; 
+  step_button[0].style.opacity = 1; 
+
+  });
+
+}
+
+
+for (var j = 0; j < step_button.length; j++) {
+
+  step_button[j].addEventListener('click', () => {
+
+
+    for (var i = 0; i < rond.length - 1; i++) {
+
+      if (rond[i].classList.contains("active")) {
+        rond[i].classList.remove("active");
+        rond[i + 1].classList.add("active");
+        steps[i].style.opacity = "0";
+        steps[i].style.pointerEvents = "none";
+        steps[i + 1].style.opacity = "1";
+        steps[i + 1].style.transform = "scale(1)";
+        steps[i + 1].style.pointerEvents = "auto";
+        
+        step_button[i].style.opacity = 0;
+        step_button[i+1].style.opacity = 1;
+        step_button[i + 1].style.pointerEvents = "auto";
+
+        document.querySelector(`[data-title="${i}"]`).style.opacity =0;
+        document.querySelector(`[data-title="${i+1}"]`).style.opacity =1;        
+        break;
+      }
+    }
+  })
+}
