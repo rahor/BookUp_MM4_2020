d514aa73ace5d54a0fa2ee3b37d534efd5e879f1
rahor <hor.raphaell@gmail.com>
2020-01-24 16:37:07 +0100

    amélioration script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
+// Declaration des elements
 const stepElems = document.getElementsByClassName("step");
 const roundStepElems = document.getElementsByClassName("tunnel_rond");
 const buttonElems = document.getElementsByClassName("bouton_confirmation");
 const stepTitle = document.getElementById("stepTitle");
 
-const stepTitles = ["Mode de retrait", "Récapitulatif", "Confirmation"];
+const stepTitles = ["Mode de retrait", "Récapitulatif", "Confirmation","Home"];
 
+// Fonction déclenchée au clic
 function clickHandler(e) {
   const target = e.target;
   const intent = parseInt(target.dataset.intent, 10);
 
   stepTitle.innerHTML = stepTitles[intent];
 
+  // On parcourt toutes les pages
   for (const step of stepElems) {
     const stepIndex = parseInt(step.dataset.step, 10);
     
@@ -21,11 +24,9 @@ function clickHandler(e) {
     }
   }
 
-
+  // Avancement dans le tunnel (rond)
   for (const roundStep of roundStepElems) {
-    console.log(roundStep)
     const stepIndex1 = parseInt(roundStep.dataset.step, 10);
-    console.log(stepIndex1)
 
     if (stepIndex1 === intent) {
       roundStep.classList.add("tunnel_rond_active");
@@ -40,68 +41,22 @@ for (const buttonElem of buttonElems) {
   buttonElem.addEventListener("click", clickHandler);
 }
 
+// Activation du boutton pour la réservation
+var retraitCarte = document.getElementsByClassName("retrait_carte");
 
+// gestion de la sélection du mode de retrait
+for (var l = 0; l < retraitCarte.length; l++) {
+  retraitCarte[l].addEventListener("click", function() {
+  var selected = document.getElementsByClassName("selected");
 
+  // Si on a sélectionné
+  if (selected.length > 0) { 
+    selected[0].className = selected[0].className.replace(" selected", "");
+  }
+  this.className += " selected";
+  buttonElems[0].classList.remove("disabled")
+  buttonElems[0].disabled = false;
 
-
-
-
-
-// var steps = [
-//   step0 = document.querySelector('[data-id="1"]'),
-//   step1 = document.querySelector('[data-id="2"]'),
-//   step2 = document.querySelector('[data-id="3"]'),
-// ]
-
-// // Rond/Etape du tunnel
-// var rond = document.getElementsByClassName('tunnel_rond');
-
-// // Boutton de confirmation
-// var step_button = document.getElementsByClassName('bouton_confirmation')
-
-// //Mode de retrait
-// var retraitCarte = document.getElementsByClassName("retrait_carte");
-
-// // gestion de la sélection du mode de retrait
-// for (var l = 0; l < retraitCarte.length; l++) {
-//   retraitCarte[l].addEventListener("click", function() {
-//   var selected = document.getElementsByClassName("selected");
-//   if (selected.length > 0) { 
-//     selected[0].className = selected[0].className.replace(" selected", "");
-//   }
-//   this.className += " selected";
-
-//   // On autorise le clic sur le bouton si un mode de retrait a été sélectionné
-//   step_button[0].style.pointerEvents = "auto"; 
-//   step_button[0].style.opacity = 1; 
-
-//   });
-
-// }
-
-
-// for (var j = 0; j < step_button.length; j++) {
-
-//   step_button[j].addEventListener('click', () => {
-
-
-//     for (var i = 0; i < rond.length - 1; i++) {
-
-//       if (rond[i].classList.contains("active")) {
-//         rond[i].classList.remove("active");
-//         rond[i + 1].classList.add("active");
-//         steps[i].style.opacity = "0"; 
-//         steps[i].style.pointerEvents = "none";
-//         steps[i + 1].style.opacity = "1";
-//         steps[i + 1].style.transform = "scale(1)";
-//         steps[i + 1].style.pointerEvents = "auto";
-        
-//         step_button[i].style.opacity = 0;
-//         step_button[i+1].style.opacity = 1;
-//         step_button[i + 1].style.pointerEvents = "auto";
-
-//         break;
-//       }
-//     }
-//   })
-// }
+}
+  )
+}
